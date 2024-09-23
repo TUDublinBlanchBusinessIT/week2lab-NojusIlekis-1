ff44884ce1bb94031727a24a7959da7a38ad689c
NojusIlekis <b00100794@mytudublin.ie>
2024-09-23 16:33:53 +0100

    part 2

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default function App(){
 
   return(
     <View>
-      <Text>Hello, World</Text>
+      <Text style={styles.paragraph}>Hello, World</Text>
     </View>
   )
-};
+}
 
